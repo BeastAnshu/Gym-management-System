da7ec8e95a3939089568a49a3436d9cdd8c9a027
BeastAnshu <anshudey2003@gmail.com>
2024-02-04 00:23:24 +0530

    Initial commit

---
+++ Gym management System/src/gym/management/system/GymManagementSystem.java
@@ -0,0 +1,17 @@
+
+package gym.management.system;
+
+/**
+ *
+ * 
+ */
+public class GymManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/gym/management/system/GymManagementSystem.java
@@ -0,0 +1,17 @@
+
+package gym.management.system;
+
+/**
+ *
+ * @author AnshuZ
+ */
+public class GymManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
